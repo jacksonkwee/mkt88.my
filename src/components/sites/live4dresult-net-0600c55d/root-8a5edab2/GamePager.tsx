85e48dc06ca7906e3b6f6ca7cc1868236ca12243
mkt88 <admin@mkt88.my>
2026-09-09 22:25:32 +0800

    Phone pager: one page per swipe (native snap, no overshoot)

--- src/components/sites/live4dresult-net-0600c55d/root-8a5edab2/GamePager.tsx
+++ src/components/sites/live4dresult-net-0600c55d/root-8a5edab2/GamePager.tsx
@@ -103,10 +103,10 @@ export default function GamePager({ initialIndex = 0, name }: { initialIndex?: n
     el.scrollBy({ left: dx < 0 ? el.clientWidth : -el.clientWidth, behavior: "smooth" });
   };
   return (
-    <div ref={track} onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}
+    <div ref={track}
       style={{ display: "flex", overflowX: "auto", overflowY: "hidden", scrollSnapType: "x mandatory", WebkitOverflowScrolling: "touch", touchAction: "pan-y", alignItems: "stretch", height: "calc(100dvh - 150px)" }}>
       {PAGES.map((pg) => (
-        <div key={pg.title} style={{ flex: "0 0 100%", scrollSnapAlign: "start", height: "100%", overflowY: "auto", WebkitOverflowScrolling: "touch", overscrollBehaviorY: "contain", padding: "4px 2px 24px" }}>
+        <div key={pg.title} style={{ flex: "0 0 100%", scrollSnapAlign: "start", scrollSnapStop: "always", height: "100%", overflowY: "auto", WebkitOverflowScrolling: "touch", overscrollBehaviorY: "contain", padding: "4px 2px 24px" }}>
           <div style={{ textAlign: "center", fontWeight: 800, color: "#cc0000", margin: "6px 0 2px", fontSize: 16 }}>{pg.title}</div>
           {pg.html ? (
             <div dangerouslySetInnerHTML={{ __html: pg.html }} />
