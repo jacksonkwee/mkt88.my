96751eaa428537fca18bec5b43f9d98d6e7a8274
mkt88 <admin@mkt88.my>
2026-09-11 10:24:55 +0800

    fix: add swipe pager and title to Singapore app page

--- src/app/singapore-4d-results/page.tsx
+++ src/app/singapore-4d-results/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Header from "../../components/sites/live4dresult-net-0600c55d/root-8a5edab2/Header";
 import RegionButtons from "../../components/sites/live4dresult-net-0600c55d/root-8a5edab2/RegionButtons";
 import SnapshotPage from "../../components/sites/live4dresult-net-0600c55d/root-8a5edab2/SnapshotPage";
+import GamePager from "../../components/sites/live4dresult-net-0600c55d/root-8a5edab2/GamePager";
 import Footer from "../../components/sites/live4dresult-net-0600c55d/root-8a5edab2/Footer";
 import raw from "../../components/sites/live4dresult-net-0600c55d/root-8a5edab2/snapshots/singapore-4d-results.content.json";
 import GoogleAdsense from "../../components/GoogleAdsense";
@@ -19,7 +20,12 @@ export default function Page() {
     <>
       <Header />
       <RegionButtons />
-      <SnapshotPage html={colHtml} />
+      <div className="d-lg-none">
+        <GamePager name="sg" />
+      </div>
+      <div className="d-none d-lg-block">
+        <SnapshotPage html={colHtml} />
+      </div>
       <SeoText
         heading="Singapore Pools 4D Results 新加坡4D開彩結果"
         paragraphs={[
